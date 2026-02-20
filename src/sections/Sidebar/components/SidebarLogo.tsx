1d5e263c7c491fb31d0902e4d02d1fccb513c511
Pedro Modesto <127983785+pedrovily1@users.noreply.github.com>
2026-02-20 18:23:50 +0100

    update

--- src/sections/Sidebar/components/SidebarLogo.tsx
+++ src/sections/Sidebar/components/SidebarLogo.tsx
@@ -2,7 +2,7 @@ export const SidebarLogo = () => {
     return (
         <div className="flex items-center justify-center py-8 border-b w-full overflow-hidden">
             <img
-                src="/public/logo.svg"
+                src="/logo.svg"
                 alt="CWS - Slovakia"
                 className="h-auto object-contain scale-[1.5] translate-x-14"
             />
